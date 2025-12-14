b3c66f1bd8031169282d4fd99bc0c994c52d4fea
Mohamed Sabry <mohamedelgamed1337@gmail.com>
2025-12-14 12:03:18 +0200

    Sign menu 1st try

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 export default function SignIn () {
   return (
+    // test
     <div className=" bg-white flex justify-center items-center min-h-screen">
       <div  className="shadow-xl  p-6 bg-gray-300 w-96">
         <label  className="text-3xl font-bold text-gray-800 block mb-6   justify-center item-center flex">Create Account</label>
@@ -12,11 +13,11 @@ export default function SignIn () {
 <label className="text-semibold text-3xl block mt-8">Gender</label>
 <select className="w-full p-4 border rounded  focus:outline-none">
   <option value="" className="">Select Gender</option>  
-  <option value="Male">Male</option>
+  <option value="Male">Male</option>``
   <option value="Female">Female</option>
 </select>
 <button className="border rounded w-full mt-4 bg-blue-500 hover:bg-blue-400 p-2 text-white">Sign In</button>
       </div>
     </div>
   )
-}
+}``
